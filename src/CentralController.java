1a0bb777ede6ae5c381b5e3a10c7420704ec4aa4
Segade <hyvoyxa@gmail.com>
2024-01-28 15:31:14 +0000

    Using Threads
    MyVoice implements Threads to try to stop the voice when the users presses the Control key. The program works similar to without this modification

--- src/CentralController.java
+++ src/CentralController.java
@@ -2,21 +2,15 @@ import javax.swing.JOptionPane;
 
 public class CentralController {
 
-CentralController(){
-MyVoice myVoice = MyVoice.getInstance();
-    myVoice.speak("Welcome to the central application");
-CentralView centralView = new CentralView(this);
-} // end constructor
-
-    public void  speak(String textToSpeech) {
+    CentralController(){
         MyVoice myVoice = MyVoice.getInstance();
-        if (textToSpeech.equals("Ctrl")){
-try {
-myVoice.interrupt();
-}catch(Exception e){System.out.println("Exception handled "+e);}
+        myVoice.speak("Welcome to the central application");
+        CentralView centralView = new CentralView(this);
+    } // end constructor
 
-    }else
-    myVoice.speak(textToSpeech);
+    public void  speak(String textToSpeech){
+        MyVoice myVoice = MyVoice.getInstance();
+        myVoice.speak(textToSpeech);
 
     } // end of text to speech
 } // end of class
--- src/MyVoice.java
+++ src/MyVoice.java
@@ -3,50 +3,53 @@ import com.sun.speech.freetts.Voice;
 import com.sun.speech.freetts.VoiceManager;
 
 
-
-public class MyVoice extends Thread  {
-private static MyVoice uniqueInstance;
-    private  VoiceManager voiceManager;
-private     Voice voice;
-
-   public boolean exit = false;
-   private String textToSpeech;
+public class MyVoice implements Runnable  {
+    private static MyVoice uniqueInstance;
+    VoiceManager voiceManager;
+    Voice voice;
+    public boolean exit = false;
+    private static Thread thread;
+    private String textToSpeech;
 
     private MyVoice()    {
 
         String voiceName = "kevin16";
-         voiceManager = VoiceManager.getInstance();
-         voice = voiceManager.getVoice(voiceName);
- voice.allocate();
-
+        voiceManager = VoiceManager.getInstance();
+        voice = voiceManager.getVoice(voiceName);
+        voice.allocate();
     } // end of constructor
 
     public static MyVoice getInstance(){
         if (uniqueInstance == null)
             uniqueInstance = new MyVoice();
-
+thread = new Thread(uniqueInstance);
 
         return  uniqueInstance;
     } // end get instance
 
     public void  speak(String textToSpeech) {
-           this.textToSpeech = textToSpeech;
+         this.textToSpeech = textToSpeech;
+         if(textToSpeech.equals("Ctrl")){
+             exit = true;
+         }
 
-     run();
+        uniqueInstance = new MyVoice();
+        thread = new Thread(uniqueInstance);
+
+        run();
+
+
     } // end speak
 
-public void  deallocate(){
+    public void  deallocate(){
         voice.deallocate();
-} // end ealocate
+    } // end deallocate
 
-public void run() {
-try {
-    Thread.sleep(1000);
-        voice.speak(textToSpeech);
-}catch(InterruptedException e){
-    throw new RuntimeException("Thread interrupted..."+e);
-
-}
-} // end run
+    public void run(){
+        while(!exit) {
+            voice.speak(textToSpeech);
+            exit = true;
+        }
+    } // end run
 
 } // end of class
